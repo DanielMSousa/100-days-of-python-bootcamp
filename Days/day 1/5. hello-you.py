9a6b79ae285c5805aa39015c3b171f04d5fd2cf7
danielmsousa <danjoje@gmail.com>
2022-04-21 10:19:34 -0300

    Initial commit

---
+++ Days/day 1/5. hello-you.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+#the input will be computed first
+#and then the compute will compute the print
+
+print("Hello " + input("What is your name? "))
---
+++ README.md
@@ -0,0 +1,2 @@
+# 100-days-of-python-bootcamp
+ The tutorials, exercises and projects done during the 100 days of python course
